f0d34828fee7deb770d25205c5dbe2b82f6580b3
saghro <ayoubsaghro27@gmail.com>
2023-12-21 00:35:25 +0100

    header2

--- src/components/header/Header2.jsx
+++ src/components/header/Header2.jsx
@@ -1,65 +1,95 @@
 import { ShoppingCartOutlined } from "@mui/icons-material";
-import { Container, InputBase, Stack, Typography, alpha, styled } from "@mui/material";
-import SearchIcon from '@mui/icons-material/Search';
+import {
+  Badge,
+  Container,
+  IconButton,
+  InputBase,
+  Stack,
+  Typography,
+  alpha,
+  styled,
+} from "@mui/material";
+import SearchIcon from "@mui/icons-material/Search";
+import Person2OutlinedIcon from "@mui/icons-material/Person2Outlined";
+import ShoppingCartIcon from "@mui/icons-material/ShoppingCart";
 
-const Search = styled('div')(({ theme }) => ({
-    position: 'relative',
-    borderRadius: theme.shape.borderRadius,
-    backgroundColor: alpha(theme.palette.common.white, 0.15),
-    '&:hover': {
-      backgroundColor: alpha(theme.palette.common.white, 0.25),
+const Search = styled("div")(({ theme }) => ({
+  position: "relative",
+  borderRadius: theme.shape.borderRadius,
+  backgroundColor: alpha(theme.palette.common.white, 0.15),
+  "&:hover": {
+    backgroundColor: alpha(theme.palette.common.white, 0.25),
+  },
+  marginRight: theme.spacing(2),
+  marginLeft: 0,
+  width: "100%",
+  [theme.breakpoints.up("sm")]: {
+    marginLeft: theme.spacing(3),
+    width: "auto",
+  },
+}));
+
+const SearchIconWrapper = styled("div")(({ theme }) => ({
+  padding: theme.spacing(0, 2),
+  height: "100%",
+  position: "absolute",
+  pointerEvents: "none",
+  display: "flex",
+  alignItems: "center",
+  justifyContent: "center",
+}));
+
+const StyledInputBase = styled(InputBase)(({ theme }) => ({
+  color: "inherit",
+  "& .MuiInputBase-input": {
+    padding: theme.spacing(1, 1, 1, 0),
+    // vertical padding + font size from searchIcon
+    paddingLeft: `calc(1em + ${theme.spacing(4)})`,
+    transition: theme.transitions.create("width"),
+    width: "100%",
+    [theme.breakpoints.up("md")]: {
+      width: "20ch",
     },
-    marginRight: theme.spacing(2),
-    marginLeft: 0,
-    width: '100%',
-    [theme.breakpoints.up('sm')]: {
-      marginLeft: theme.spacing(3),
-      width: 'auto',
-    },
-  }));
-  
-  const SearchIconWrapper = styled('div')(({ theme }) => ({
-    padding: theme.spacing(0, 2),
-    height: '100%',
-    position: 'absolute',
-    pointerEvents: 'none',
-    display: 'flex',
-    alignItems: 'center',
-    justifyContent: 'center',
-  }));
-  
-  const StyledInputBase = styled(InputBase)(({ theme }) => ({
-    color: 'inherit',
-    '& .MuiInputBase-input': {
-      padding: theme.spacing(1, 1, 1, 0),
-      // vertical padding + font size from searchIcon
-      paddingLeft: `calc(1em + ${theme.spacing(4)})`,
-      transition: theme.transitions.create('width'),
-      width: '100%',
-      [theme.breakpoints.up('md')]: {
-        width: '20ch',
-      },
-    },
-  }));
+  },
+}));
+
+const StyledBadge = styled(Badge)(({ theme }) => ({
+  "& .MuiBadge-badge": {
+    right: -3,
+    top: 13,
+    border: `2px solid ${theme.palette.background.paper}`,
+    padding: "0 4px",
+  },
+}));
 
 const Header2 = () => {
-    return (
-         <Container sx={{my:3}}>
-            <Stack alignItems={"center"}>
-                <ShoppingCartOutlined />
-                    <Typography variant="body2"> AGAFAY STORE</Typography>
-            </Stack>
-            <Search>
-            <SearchIconWrapper>
-              <SearchIcon />
-            </SearchIconWrapper>
-            <StyledInputBase
-              placeholder="Search…"
-              inputProps={{ 'aria-label': 'search' }}
-            />
-          </Search>
-        </Container>
-    );
-}
+  return (
+    <Container sx={{ my: 3, display: "flex", justifyContent: "space-between" }}>
+      <Stack alignItems={"center"}>
+        <ShoppingCartOutlined />
+        <Typography variant="body2"> AGAFAY STORE</Typography>
+      </Stack>
+      <Search sx={{borderRadius:"22px"}}>
+        <SearchIconWrapper>
+          <SearchIcon />
+        </SearchIconWrapper>
+        <StyledInputBase
+          placeholder="Search…"
+          inputProps={{ "aria-label": "search" }}
+        />
+      </Search>
+      <Stack direction={"row"} alignItems={"center"}>
+        <IconButton aria-label="cart">
+          <StyledBadge badgeContent={4} color="primary">
+            <ShoppingCartIcon />
+          </StyledBadge>
+        </IconButton>
+        <IconButton>
+          <Person2OutlinedIcon />
+        </IconButton>
+      </Stack>
+    </Container>
+  );
+};
 
 export default Header2;
