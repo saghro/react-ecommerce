99326dc1116c666b9d0c6c5b26d0cceb11d12a3a
saghro <ayoubsaghro27@gmail.com>
2023-12-21 01:48:52 +0100

    add search menu

--- src/components/header/Header2.jsx
+++ src/components/header/Header2.jsx
@@ -1,17 +1,14 @@
 import { ShoppingCartOutlined } from "@mui/icons-material";
-import {
-  Badge,
-  Container,
-  IconButton,
-  InputBase,
-  Stack,
-  Typography,
-  alpha,
-  styled,
-} from "@mui/material";
+import {Badge,Container,IconButton,InputBase,Stack,Typography,alpha,styled,} from "@mui/material";
 import SearchIcon from "@mui/icons-material/Search";
 import Person2OutlinedIcon from "@mui/icons-material/Person2Outlined";
 import ShoppingCartIcon from "@mui/icons-material/ShoppingCart";
+import List from '@mui/material/List';
+import ListItem from '@mui/material/ListItem';
+import ListItemText from '@mui/material/ListItemText';
+import MenuItem from '@mui/material/MenuItem';
+import Menu from '@mui/material/Menu';
+import { useState } from "react";
 
 const Search = styled("div")(({ theme }) => ({
   position: "relative",
@@ -61,15 +58,37 @@ const StyledBadge = styled(Badge)(({ theme }) => ({
     padding: "0 4px",
   },
 }));
+const options = [
+    'Show some love to MUI',
+    'Show all notification content',
+    'Hide sensitive notification content',
+    'Hide all notification content',
+  ];
 
 const Header2 = () => {
+    const [anchorEl, setAnchorEl] = useState(null);
+    const [selectedIndex, setSelectedIndex] = useState(1);
+    const open = Boolean(anchorEl);
+    const handleClickListItem = (event) => {
+      setAnchorEl(event.currentTarget);
+    };
+  
+    const handleMenuItemClick = (event, index) => {
+      setSelectedIndex(index);
+      setAnchorEl(null);
+    };
+  
+    const handleClose = () => {
+      setAnchorEl(null);
+    };
+  
   return (
     <Container sx={{ my: 3, display: "flex", justifyContent: "space-between" }}>
       <Stack alignItems={"center"}>
         <ShoppingCartOutlined />
         <Typography variant="body2"> AGAFAY STORE</Typography>
       </Stack>
-      <Search sx={{borderRadius:"22px"}}>
+      <Search sx={{ borderRadius: "22px" }}>
         <SearchIconWrapper>
           <SearchIcon />
         </SearchIconWrapper>
@@ -77,6 +96,48 @@ const Header2 = () => {
           placeholder="Search…"
           inputProps={{ "aria-label": "search" }}
         />
+
+        <List
+          component="nav"
+          aria-label="Device settings"
+          sx={{ bgcolor: "background.paper" }}
+        >
+          <ListItem
+            button
+            id="lock-button"
+            aria-haspopup="listbox"
+            aria-controls="lock-menu"
+            aria-label="when device is locked"
+            aria-expanded={open ? "true" : undefined}
+            onClick={handleClickListItem}
+          >
+            <ListItemText
+              primary="When device is locked"
+              secondary={options[selectedIndex]}
+            />
+          </ListItem>
+        </List>
+        <Menu
+          id="lock-menu"
+          anchorEl={anchorEl}
+          open={open}
+          onClose={handleClose}
+          MenuListProps={{
+            "aria-labelledby": "lock-button",
+            role: "listbox",
+          }}
+        >
+          {options.map((option, index) => (
+            <MenuItem
+              key={option}
+              disabled={index === 0}
+              selected={index === selectedIndex}
+              onClick={(event) => handleMenuItemClick(event, index)}
+            >
+              {option}
+            </MenuItem>
+          ))}
+        </Menu>
       </Search>
       <Stack direction={"row"} alignItems={"center"}>
         <IconButton aria-label="cart">
