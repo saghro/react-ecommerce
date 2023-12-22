e3bd8b9b50722b9773eb153edbd8b05c7be829bc
saghro <ayoubsaghro27@gmail.com>
2023-12-22 21:23:58 +0100

    styler menu

--- src/components/header/Header3.jsx
+++ src/components/header/Header3.jsx
@@ -1,54 +1,82 @@
 import { KeyboardArrowRightOutlined } from "@mui/icons-material";
 import WindowIcon from "@mui/icons-material/Window";
-import { Box, Button, Container, Menu, Typography, useTheme } from "@mui/material";
-import MenuItem from '@mui/material/MenuItem';
+import Menu from "@mui/material/Menu";
+import MenuIcon from "@mui/icons-material/Menu";
+import { Box, Button, Container, IconButton, Typography, useTheme } from "@mui/material";
+import MenuItem from "@mui/material/MenuItem";
 import { useState } from "react";
+import{
+    SportsEsportsOutlined,
+    ElectricBikeOutlined,
+    LaptopChromebookOutlined,
+    MenuBookOutlined,
+} from "@mui/icons-material"
+
+
 const Header3 = () => {
-    const [anchorEl, setAnchorEl] =useState(null);
-    const open = Boolean(anchorEl);
-    const handleClick = (event) => {
-      setAnchorEl(event.currentTarget);
-    };
-    const handleClose = () => {
-      setAnchorEl(null);
-    };
-    const theme = useTheme()
+  const [anchorEl, setAnchorEl] = useState(null);
+  const open = Boolean(anchorEl);
+  const handleClick = (event) => {
+    setAnchorEl(event.currentTarget);
+  };
+  const handleClose = () => {
+    setAnchorEl(null);
+  };
+  const theme = useTheme();
   return (
-    <Container>
-      <Button
-        id="basic-button"
-        aria-controls={open ? "basic-menu" : undefined}
-        aria-haspopup="true"
-        aria-expanded={open ? "true" : undefined}
-        onClick={handleClick}
-        sx={{width:222}}
-      >
-        <WindowIcon />
-        <Typography  
-           sx={{
-            padding:"0",
-            textTransform:"capitalize",
-            mx:1,
-           }}
+    <Container
+      sx={{
+        display: "flex",
+        alignItems: "center",
+        justifyContent: "space-between",
+      }}
+    >
+      <Box>
+        <Button
+          id="basic-button"
+          aria-controls={open ? "basic-menu" : undefined}
+          aria-haspopup="true"
+          aria-expanded={open ? "true" : undefined}
+          onClick={handleClick}
+          
+          sx={{
+            width: 222,
+            // @ts-ignore
+            bgcolor: theme.palette.myColor.main,
+            color: theme.palette.text.secondary,
+          }}
         >
+          <WindowIcon />
+          <Typography
+            sx={{
+              padding: "0",
+              textTransform: "capitalize",
+              mx: 1,
+            }}
+          >
             Categories
-        </Typography>
-        <Box flexGrow={1} />
-        <KeyboardArrowRightOutlined />
-      </Button>
-      <Menu
-        id="basic-menu"
-        anchorEl={anchorEl}
-        open={open}
-        onClose={handleClose}
-        MenuListProps={{
-          "aria-labelledby": "basic-button",
-        }}
-      >
-        <MenuItem onClick={handleClose}>Profile</MenuItem>
-        <MenuItem onClick={handleClose}>My account</MenuItem>
-        <MenuItem onClick={handleClose}>Logout</MenuItem>
-      </Menu>
+          </Typography>
+          <Box flexGrow={1} />
+          <KeyboardArrowRightOutlined />
+        </Button>
+        <Menu
+          id="basic-menu"
+          anchorEl={anchorEl}
+          open={open}
+          onClose={handleClose}
+          MenuListProps={{
+            "aria-labelledby": "basic-button",
+          }}
+        >
+          <MenuItem onClick={handleClose}> <SportsEsportsOutlined /> Profile</MenuItem>
+          <MenuItem onClick={handleClose}>My account</MenuItem>
+          <MenuItem onClick={handleClose}>Logout</MenuItem>
+        </Menu>
+      </Box>
+
+      <IconButton>
+         <MenuIcon /> 
+      </IconButton>
     </Container>
   );
 };
