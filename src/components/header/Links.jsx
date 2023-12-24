9c1efc6045a38872d3bdd7f803994f55dca4ce76
saghro <ayoubsaghro27@gmail.com>
2023-12-24 21:02:50 +0100

    menu ico

--- src/components/header/Links.jsx
+++ src/components/header/Links.jsx
@@ -1,16 +1,79 @@
-import { ExpandMore } from '@mui/icons-material';
-import { Box, Typography } from '@mui/material';
-import React from 'react';
+import { ExpandMore } from "@mui/icons-material";
+import {
+  Box,
+  List,
+  ListItem,
+  ListItemButton,
+  ListItemText,
+  Paper,
+  Typography,
+} from "@mui/material";
 
 const Links = () => {
-    return (
-        <Box sx={{display:"flex",alignItems:"center"}}>
-            <Typography variant='body1'>
-                 Home
-            </Typography>
-            <ExpandMore sx={{fontSize:"16px",ml:1}}/>
-        </Box>
-    );
-}
+  return (
+    <Box
+      sx={{
+        ":hover .show-when-hover": { display: "block" },
+        position: "relative",
+        display: "flex",
+        alignItems: "center",
+      }}
+    >
+      <Typography variant="body1">Home</Typography>
+      <ExpandMore sx={{ fontSize: "16px", ml: 1 }} />
+
+      <Box
+        className="show-when-hover"
+        sx={{
+          position: "absolute",
+          top: "100%",
+          minWidth: "170px",
+          left: "50%",
+          transform: " translateX(-50%)",
+          display: "none",
+        }}
+      >
+        <Paper sx={{ mt: 2 }}>
+          <nav aria-label="secondary mailbox folders">
+            <List>
+              <ListItem disablePadding>
+                <ListItemButton sx={{display:"flex"}}>
+                  <ListItemText
+                    sx={{ ".MuiTypography-root": { fontSize: "15px" } }}
+                    primary="Trash"
+                  />
+                </ListItemButton>
+              </ListItem>
+              <ListItem disablePadding>
+                <ListItemButton>
+                  <ListItemText
+                    sx={{ ".MuiTypography-root": { fontSize: "15px" } }}
+                    primary="Trash"
+                  />
+                </ListItemButton>
+              </ListItem>
+              <ListItem disablePadding>
+                <ListItemButton>
+                  <ListItemText
+                    sx={{ ".MuiTypography-root": { fontSize: "15px" } }}
+                    primary="Trash"
+                  />
+                </ListItemButton>
+              </ListItem>
+              <ListItem disablePadding>
+                <ListItemButton>
+                  <ListItemText
+                    sx={{ ".MuiTypography-root": { fontSize: "15px" } }}
+                    primary="Trash"
+                  />
+                </ListItemButton>
+              </ListItem>
+            </List>
+          </nav>
+        </Paper>
+      </Box>
+    </Box>
+  );
+};
 
 export default Links;
