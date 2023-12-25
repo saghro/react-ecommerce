61926b2b3bed855663a5916f727c85a2e9dcd2b0
saghro <ayoubsaghro27@gmail.com>
2023-12-25 23:09:07 +0100

    ajouter imgs

--- src/components/hero/Hero.jsx
+++ src/components/hero/Hero.jsx
@@ -1,9 +1,21 @@
+import { Box, Container } from "@mui/material";
+
 
 const Hero = () => {
     return (
-        <div>
-             
-        </div>
+        <Container sx={{mt:2.5,display:"flex",alignItems:"center"}}>
+            <Box>
+                Slider
+            </Box>
+            <Box>
+                <Box>
+                    <img src="" alt="" />
+                </Box>
+                <Box>
+                    <img src="" alt="" />
+                </Box>
+            </Box>
+        </Container>
     );
 }
 
