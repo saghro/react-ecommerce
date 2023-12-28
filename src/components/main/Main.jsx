289dce1a4ffad711ed589ad0d76b12576db8a3f7
saghro <ayoubsaghro27@gmail.com>
2023-12-28 21:31:38 +0100

    add card

--- src/components/main/Main.jsx
+++ src/components/main/Main.jsx
@@ -1,24 +1,28 @@
-import { Box, Container, Stack, Typography } from "@mui/material";
-import FormatAlignLeftIcon from '@mui/icons-material/FormatAlignLeft';
-import FormatAlignCenterIcon from '@mui/icons-material/FormatAlignCenter';
-import FormatAlignRightIcon from '@mui/icons-material/FormatAlignRight';
-import FormatAlignJustifyIcon from '@mui/icons-material/FormatAlignJustify';
-import ToggleButton from '@mui/material/ToggleButton';
-import ToggleButtonGroup from '@mui/material/ToggleButtonGroup';
+import { Box, Container, Stack, Typography, useTheme } from "@mui/material";
+import ToggleButton from "@mui/material/ToggleButton";
+import ToggleButtonGroup from "@mui/material/ToggleButtonGroup";
 import React from "react";
+import Card from '@mui/material/Card';
+import CardActions from '@mui/material/CardActions';
+import CardContent from '@mui/material/CardContent';
+import CardMedia from '@mui/material/CardMedia';
+import Button from '@mui/material/Button';
 
 const Main = () => {
-    const [alignment, setAlignment] = React.useState('left');
+  const [alignment, setAlignment] = React.useState("left");
 
-    const handleAlignment = (event, newAlignment) => {
-      setAlignment(newAlignment);
-    };
-  
+  const handleAlignment = (event, newAlignment) => {
+    setAlignment(newAlignment);
+  };
+  const theme = useTheme();
   return (
     <Container>
-      <Stack direction={"row"}
-      alignItems={"center"}
-       justifyContent={"space-between"}
+      <Stack
+        direction={"row"}
+        alignItems={"center"}
+        justifyContent={"space-between"}
+        flexWrap={"wrap"}
+        gap={3}
       >
         <Box>
           <Typography variant="h6">Selected Products</Typography>
@@ -33,24 +37,60 @@ const Main = () => {
           onChange={handleAlignment}
           aria-label="text alignment"
           sx={{
-            ".Mui-selected":{
-              border:"1px solid rgba(233,69,96,0.5) !important",
-              color:"#e94560",
-              backgroundColor:"initial"
-            }
+            ".Mui-selected": {
+              border: "1px solid rgba(233,69,96,0.5) !important",
+              color: "#e94560",
+              backgroundColor: "initial",
+            },
           }}
         >
-          <ToggleButton className="myButton" value="left" aria-label="left aligned">
+          <ToggleButton
+            sx={{ color: theme.palette.text.primary }}
+            className="myButton"
+            value="left"
+            aria-label="left aligned"
+          >
             All Products
           </ToggleButton>
-          <ToggleButton sx={{mx:"16px !important"}} className="myButton" value="center" aria-label="centered">
+          <ToggleButton
+            sx={{ mx: "16px !important", color: theme.palette.text.primary }}
+            className="myButton"
+            value="center"
+            aria-label="centered"
+          >
             Men Category
           </ToggleButton>
-          <ToggleButton className="myButton" value="right" aria-label="right aligned">
+          <ToggleButton
+            sx={{ color: theme.palette.text.primary }}
+            className="myButton"
+            value="right"
+            aria-label="right aligned"
+          >
             Women Category
           </ToggleButton>
-         
         </ToggleButtonGroup>
+      </Stack>
+      <Stack>
+        <Card sx={{ maxWidth: 345 }}>
+          <CardMedia
+            sx={{ height: 140 }}
+            image="/static/images/cards/contemplative-reptile.jpg"
+            title="green iguana"
+          />
+          <CardContent>
+            <Typography gutterBottom variant="h5" component="div">
+              Lizard
+            </Typography>
+            <Typography variant="body2" color="text.secondary">
+              Lizards are a widespread group of squamate reptiles, with over
+              6,000 species, ranging across all continents except Antarctica
+            </Typography>
+          </CardContent>
+          <CardActions>
+            <Button size="small">Share</Button>
+            <Button size="small">Learn More</Button>
+          </CardActions>
+        </Card>
       </Stack>
     </Container>
   );
